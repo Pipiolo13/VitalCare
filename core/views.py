db05c116c43d92988183c0e65853b4383ead6d7c
MayuLizbeth <mayuliz44g@gmail.com>
2021-01-19 19:26:38 -0500

    MG VITALCORE TEST1

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.....
+
+
+html_cabecera =  """
+    <h1>WELCOME</h1>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/Doctores">DOCTORES</a></li>
+        
+
+    </ul>
+""" 
+def  home ( inicio ):
+    return  HttpResponse ( "<h1> SISTEMA VITALCARE </h1>" )
+
+
+def  Doctores ( doctores ):
+    return  HttpResponse ( "<h1> HISTORIAL CLINICO DE PACIENTES </h1>" )
