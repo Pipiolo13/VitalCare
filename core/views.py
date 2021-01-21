316515d3224913da8d56123c7a162e3e88766ee8
MayuLizbeth <mayuliz44g@gmail.com>
2021-01-21 00:35:25 -0500

    MG VITALCARE PLANTILLA BASE DOCTOR

--- core/views.py
+++ core/views.py
@@ -3,18 +3,5 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.....
 
 
-html_cabecera =  """
-    <h1>WELCOME</h1>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/Doctores">DOCTORES</a></li>
-        
-
-    </ul>
-""" 
-def  home ( inicio ):
-    return  HttpResponse ( "<h1> SISTEMA VITALCARE </h1>" )
-
-
-def  Doctores ( doctores ):
-    return  HttpResponse ( "<h1> HISTORIAL CLINICO DE PACIENTES </h1>" )
+def doctor (request):
+    return  render(request,"core/BaseDoctor.html")
