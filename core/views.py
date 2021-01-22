246bf8e522346ae4e7ad25951c2464c29a61870c
HENRY <77517361+HENEYR@users.noreply.github.com>
2021-01-22 01:20:09 -0500

    Add files via upload

--- core/views.py
+++ core/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
 def enfermeria(request):
-    return HttpResponse("<h1>Modulo de enfermeria</h1>")
+    return render(request, "core/enfermeria.html")    
